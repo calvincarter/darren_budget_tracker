bc187eb9de932d9b69cb4fb11e85526df12b41e2
Calvin Carter <calvincarter@pingidentity.com>
2021-03-08 13:55:25 -0600

    update service worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -2,10 +2,8 @@ const FILES_TO_CACHE = [
   "/", 
   "/index.html", 
   "/index.js",
-  "/favicon.ico", 
   "/styles.css", 
-  "/icons/icon-128x128.png", 
-  "/icons/icon-256x256.png",
+  "/icons/icon-192x192.png", 
   "/icons/icon-512x512.png"
 ];
 
